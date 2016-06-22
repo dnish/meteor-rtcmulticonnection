5c77055dffbfd69e07f9c1f7141f7b1615e31988
dennis <harbecke.dennis@gmail.com>
2016-06-22 19:11:43 +0200

    init

--- client/main.js
+++ client/main.js
@@ -1,21 +1,71 @@
-
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 
 import './main.html';
 
 import 'rtcmulticonnection-v3/RTCMultiConnection';
+import adapter from 'webrtc-adapter/out/adapter';
+
+Template.hello.viewmodel({
+
+    peerId: null,
+    callUser: null,
 
 
-Template.hello.onCreated(function helloOnCreated() {
+    call() {
+
+        myConnection.connect(this.callUser());
+
+    },
+
+    onRendered() {
+
+        window.adapter = adapter;
+        myConnection = new RTCMultiConnection();
+
+        myConnection.session = {
+            audio: true,
+            video: true,
+            data: true
+        };
+
+        myConnection.sdpConstraints.mandatory = {
+            OfferToReceiveAudio: true,
+            OfferToReceiveVideo: true
+        };
+
+        
+
+        myConnection.iceServers = [{urls:'stun:stun.l.google.com:19302'}];
+
+        myConnection.socketURL = "https://rtcmulticonnection.herokuapp.com:443/"; //Replace with your own socket server
+        myConnection.connectSocket();
+
+        //Getting own user id - don't be confused, this.peerId(value) is how we set reactive variables with ViewModel - use it instead of pure Blaze ;)
+
+        this.peerId(myConnection.userid);
 
 
 
-  myConnection = new RTCMultiConnection();
-  myConnection.socketURL = "URL TO YOUR SOCKET SERVER";
-  myConnection.connectSocket();
 
 
 
+        //Now we set also some RTCMultiConnection events
+
+        myConnection.onstream = function(e) {
+            
+            if (e.type == 'remote') {
+
+                //Stream of partner
+
+                $("#foreign").attr('src', window.URL.createObjectURL(e.stream));
+
+            } else {
+
+                //Own stream
+                $("#ownvideo").attr('src', window.URL.createObjectURL(e.stream));
+            }
+
+        }
+
+    }
 });
-
-
